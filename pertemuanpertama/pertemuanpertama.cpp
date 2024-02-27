5ce03095c9adc017695e10dd928b8b7431b59b72
arifwicakson0 <wicaksonoarif487@gmail.com>
2024-02-27 18:50:24 +0700

    Add project files.

---
+++ pertemuanpertama/pertemuanpertama.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{//begin
+// numeric njejari, nluas
+// disply `Masukan jejari= `
+// accept njejari
+// compute nLuas = 3,14 * r * r 
+//disply `luasnya= ` + nluas
+
+
+	float njejari, nluas;
+	cout << "Masukan jejari = ";
+	cin >> njejari;
+	nluas = 3.14 * njejari * njejari;
+	cout << "Luasnya = " << nLuas;
